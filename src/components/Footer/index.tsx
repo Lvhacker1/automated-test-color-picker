1b24d7019fa0b09cd8918837afd796219b3bd9a2
Lvhacker1 <lavasha2@hotmail.com>
2025-10-06 00:58:03 +0200

    stylechange

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="p-2 text-center text-sm lg:w-[1000px] lg:m-auto lg:p-4 border-t-1">
+        <footer className="p-2 text-center text-sm lg:w-[1000px] lg:m-auto lg:p-4">
             <p>vic: 2025</p>
         </footer>
     )
